6fb999ae7a79d02bee1a4d53030c94e2b72aa407
Nash Peterson <npeters021@gmail.com>
2024-09-03 16:27:56 -0500

    adding additional font elements & css files

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,12 +14,12 @@ export default function AboutPage() {
                 </div>
 
                 <div className="bio">
-                    <p>Hello! I’m Nash Peterson, an aspiring software engineer with a passion for <p className="emph">efficiency</p> and <p className="emph">creative problem solving.</p>
+                    <p>Hello! I’m Nash Peterson, an aspiring software engineer with a passion for <span className="emph">efficiency</span> and <span className="emph">creative problem solving.</span>
                     </p>
-                    <p>I recently completed a bootcamp at the <p className="emph">University of Minnesota</p>, where I honed my coding skills and developed a strong foundation in software development.</p>
-                    <p>Before diving into the tech world, I received my <p className="emph">bachelors degree</p> in <p className="emph">supply chain management</p> and spent several years in <p className="emph">logistics.</p></p>
-                    <p>Currently, I'm working as <p className="emph">Creative Director</p> of <p className="emph">Fulcrum Comiks LLC</p>, leveraging my <p className="emph">creativity</p> and <p className="emph">leadership abilities</p> to remotely coordinate artists to create my comic series: <a className="website" href="https://odysseyofthepsygonauts.com"><i>Odyssey of the Psygonauts</i></a>.</p>
-                    <p>I pride myself on being a <p className="emph">self-motivated</p>, <p className="emph">independent</p> worker with a <p className="emph">strong work ethic</p>. I believe in the importance of collaboration and have a proven track record of working effectively across departments to achieve common goals. I’m excited to transition into the tech industry to work on exciting new projects and continue expanding my skill set as a developer.</p>
+                    <p>I recently completed a bootcamp at the <span className="emph">University of Minnesota</span>, where I honed my coding skills and developed a strong foundation in software development.</p>
+                    <p>Before diving into the tech world, I received my <span className="emph">bachelors degree</span> in <span className="emph">supply chain management</span> and spent several years in <span className="emph">logistics.</span></p>
+                    <p>Currently, I'm working as <span className="emph">Creative Director</span> of <span className="emph">Fulcrum Comiks LLC</span>, leveraging my <span className="emph">creativity</span> and <span className="emph">leadership abilities</span> to remotely coordinate artists to create my comic series: <a className="website" href="https://odysseyofthepsygonauts.com"><i>Odyssey of the Psygonauts</i></a>.</p>
+                    <p>I pride myself on being a <span className="emph">self-motivated</span>, <span className="emph">independent</span> worker with a <span className="emph">strong work ethic</span>. I believe in the importance of collaboration and have a proven track record of working effectively across departments to achieve common goals. I’m excited to transition into the tech industry to work on exciting new projects and continue expanding my skill set as a developer.</p>
                 </div>
             </div>
         </>
